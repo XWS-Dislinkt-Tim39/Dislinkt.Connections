170324da136ee336a1f6894ed5e7f3e39c4cb89f
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-08-26 02:20:22 +0200

    added reject follow request

---
+++ Dislinkt.Connections.Application/RejectFollow/Commands/RejectFollowCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dislinkt.Connections.Application.RejectFollow.Commands
+{
+    public class RejectFollowCommand : IRequest<bool>
+    {
+
+        public RejectFollowCommand(ConnectionData connectionData)
+        {
+            Request = connectionData;
+        }
+        public ConnectionData Request { get; set; }
+    }
+}
---
+++ Dislinkt.Connections.Application/RejectFollow/Commands/RejectFollowHandler.cs
@@ -0,0 +1,38 @@
+﻿using Dislinkt.Connections.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dislinkt.Connections.Application.RejectFollow.Commands
+{
+    public class RejectFollowHandler : IRequestHandler<RejectFollowCommand, bool>
+    {
+        private readonly IConnectionsRepository _connectionsRepository;
+
+        public RejectFollowHandler(IConnectionsRepository connectionsRepository)
+        {
+            _connectionsRepository = connectionsRepository;
+        }
+
+        public async Task<bool> Handle(RejectFollowCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Guid sourceId = Guid.Parse(request.Request.SourceId);
+                Guid targetId = Guid.Parse(request.Request.TargetId);
+                await _connectionsRepository.RemoveConnectionAsync(targetId, sourceId, "FOLLOW_REQUEST");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
